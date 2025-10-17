56ad3626abe014fe255dcb0a86deb06c256613a1
MisterMiker <miguelrlopez2005@gmail.com>
2025-10-16 23:47:12 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -19,7 +19,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
  
  st.subheader("Conversión de texto a voz")
- image = Image.open('txt_to_audio2.png')
+ image = Image.open('1.png')
  st.image(image, width=190)
  st.write("Aplicación para presentar auditivamente un texto") 
  url = "https://mwcvogxsfresrryrvr8akq.streamlit.app/"
