36bf1ae9e0749fa11fc0ba1c64c76455abed93e7
MisterMiker <miguelrlopez2005@gmail.com>
2025-10-16 23:44:10 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -21,8 +21,8 @@ with col1:
  st.subheader("Conversión de texto a voz")
  image = Image.open('txt_to_audio2.png')
  st.image(image, width=190)
- st.write("En la siguiente enlace usaremos una de las aplicaciones de Inteligencia Artificial") 
- url = "https://imultimod.streamlit.app/"
+ st.write("Aplicación para presentar auditivamente un texto") 
+ url = "https://mwcvogxsfresrryrvr8akq.streamlit.app/"
  st.write(f"Texto a voz: [Enlace]({url})")
 
  st.subheader("Reconocimiento de Objetos")
